243e4ed274cd85919148ed1bac4faf09a214fde6
viniciusgasparino <viniciusgasparino@yahoo.com.br>
2022-06-20 20:08:05 -0300

    Criado component de botão

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,38 @@
 import Title from "../src/components/title/Title"
+import Button from "../src/components/button/Button"
+import {useState} from "react"
 
 function HomePage () {
+  const [startTime,setStarTime] = useState("00:00:00")
+  const [show, setSwow] = useState("")
+
+  function handleStart(){
+    console.log("Testando")
+  }
+
+  function handlePouse(){
+    alert("Ola mundo")
+  }
+
+  function handleZero(){
+    console.log("Testando")
+  }
+
+
+
   return (
     <>
       <Title title="Conômetro"></Title>
       <div className="container">
          <div className="center">
-            <Title title="00:00:00"></Title>
+            <Title title={startTime}></Title>
+            <Button onClick={handleStart} text="Começar"></Button>
+            <Button onClick={handlePouse} text="Pausar"></Button>
+            <Button onClick={handleZero} text="Zerar"></Button>
          </div> 
+      </div>
+      <div>
+          {show}
       </div>
     </>
   )
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,14 @@
+import styles from "./Button.module.css"
+
+function Button ({text, onClick}) {
+  return(
+    <>
+      <button onClick={onClick} className={styles.button}>
+        {text}
+      </button>
+    </>
+  )
+}
+
+
+export default Button
